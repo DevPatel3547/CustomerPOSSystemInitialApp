f98cd9190491f778e9287551d1606c624a98676c
jack-payne <112025267+jack-payne@users.noreply.github.com>
2023-10-02 15:39:30 -0700

    Add files via upload

--- inventory.py
+++ inventory.py
@@ -11,6 +11,23 @@ inventory_items = [
     {"Name": "Pearls", "Category": "Topping", "Quantity": 100, "Price": 0.50},
     {"Name": "Cups", "Category": "Disposable", "Quantity": 500, "Price": 0.10},
     {"Name": "Straws", "Category": "Disposable", "Quantity": 1000, "Price": 0.05},
+
+    {"Name": "Napkins", "Category": "Disposable", "Quantity": 5000, "Price": 0.02},
+    {"Name": "Jelly", "Category": "Topping", "Quantity": 200, "Price": 6.00},
+    {"Name": "Pudding", "Category": "Topping", "Quantity": 150, "Price": 5.50},
+    {"Name": "Grass Jelly", "Category": "Topping", "Quantity": 250, "Price": 2.00},
+    {"Name": "Red Bean", "Category": "Topping", "Quantity": 300, "Price": 3.23},
+    {"Name": "Lychee Fruits", "Category": "Topping", "Quantity": 90, "Price": 7.05},
+    {"Name": "Toilet Paper", "Category": "Disposable", "Quantity": 2000, "Price": 0.10},
+    {"Name": "Dome Lid", "Category": "Disposable", "Quantity": 1000, "Price": 0.04},
+    {"Name": "Sealing Film", "Category": "Disposable", "Quantity": 6000, "Price": 0.05},
+    {"Name": "Bubble Tea Syrup", "Category": "Ingredient", "Quantity": 4000, "Price": 1.00},
+    {"Name": "Bubble Tea Creamer", "Category": "Ingredient", "Quantity": 2000, "Price": 2.00},
+    {"Name": "Sanitation Wipes", "Category": "Disposable", "Quantity": 3000, "Price": 0.50},
+    {"Name": "Trash Bags", "Category": "Disposable", "Quantity": 400, "Price": 0.70},
+    {"Name": "Brown Sugar", "Category": "Ingredient", "Quantity": 400, "Price": 6.50},
+    {"Name": "Strawberries", "Category": "Ingredient", "Quantity": 100, "Price": 5.45},
+    {"Name": "Organic Milk", "Category": "Ingredient", "Quantity": 70, "Price": 9.05},
     # will need to add more items
 ]
 
