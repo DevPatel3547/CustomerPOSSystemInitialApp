a6ef72797bebdfb1e68de51cecc66ecc8ffba57a
Shreyas <sk.dev.tamu@gmail.com>
2023-10-09 11:50:04 -0500

    Added the functionality of JDBC and the ability to connect to the database

---
+++ Database.java
@@ -0,0 +1,45 @@
+import java.sql.*;
+import javax.naming.spi.DirStateFactory.Result;
+
+public class Database {
+    private static Connection conn = null;
+
+    public static void openJDBC() {
+        String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_10b_db";
+
+        try {
+            conn = DriverManager.getConnection(dbConnectionString, "csce315_910_devrpatel04", "password");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+    public static void closeJDBC() {
+        try {
+            conn.close();
+            System.out.println("Connection Closed.");
+        } catch(Exception e) {
+            System.out.println("Connection NOT Closed.");
+        }
+    }
+
+    public static void main(String args[]) {
+        openJDBC();
+
+        try {
+            Statement createStmt = conn.createStatement();
+            ResultSet result = conn.createStatement().executeQuery("SELECT COUNT(*) FROM OrderHistory WHERE Date = '2023-09-30';");
+            result.next();
+            System.out.println(result.getInt(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+
+        closeJDBC();
+    }
+}
---
+++ Login.java
@@ -0,0 +1,18 @@
+import java.awt.*;  
+import javax.swing.*;  
+
+public class Login {
+
+    Login() {
+
+        JFrame frame = new JFrame("Login");
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+    }
+
+    public static void main(String args[]) {
+        new Login();
+    }
+
+}
