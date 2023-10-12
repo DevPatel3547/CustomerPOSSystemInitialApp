324ae5262c6876798887c5dd249a3a64a8382acc
jack-payne <112025267+jack-payne@users.noreply.github.com>
2023-10-11 17:32:02 -0700

    Add files via upload
    New queries for the GUI test and interface. A query that looks at the menu table and gets every drink, and another one that gets the price of every drink.

--- Queries.sql
+++ Queries.sql
@@ -48,3 +48,9 @@ ORDER BY WeekStart DESC;
 
 -- Query 17 Find the Item and Quantity of a single row
 SELECT Name, Quantity FROM Inventory LIMIT 1 OFFSET 2;
+
+-- Query 18  looks at the menu table and gets every drink
+SELECT "Name of Item" AS Drink, "Cost of Item" AS Price, "Numbers Sold During Day" AS SoldDuringDay FROM menu WHERE Type = 'Drink';
+
+-- Query 19 gets the price of every drink
+SELECT "Name of Item" AS Drink, "Cost of Item" AS Price FROM menu WHERE Type = 'Drink';
