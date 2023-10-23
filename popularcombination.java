d81eeffee20148850274168f73fa45908d9cb039
DevPatel3547 <101240203+DevPatel3547@users.noreply.github.com>
2023-10-22 19:29:07 -0500

    Resotred popularcombination.java

--- popularcombination.java
+++ popularcombination.java
@@ -16,7 +16,7 @@ class PopularCombination extends JFrame {
         database = new Database();
         database.openJDBC();
 
-        String[] columnNames = {"Flavor 1 & Topping 1", "Flavor 2 & Topping 2", "Quantity"};
+        String[] columnNames = {"Flavor", "Topping", "Frequency"};
         Object[][] data = fetchDataFromDatabase(startDate, endDate);
         table = new JTable(data, columnNames);
 
@@ -41,49 +41,44 @@ class PopularCombination extends JFrame {
 
     private Object[][] fetchDataFromDatabase(String startDate, String endDate) {
         try {
-            String sql = 
-                "WITH ToppingsExploded AS (" +
-                "    -- Exploding the toppings into individual rows" +
-                "    SELECT date, flavor, UNNEST(STRING_TO_ARRAY(toppings, ', ')) AS topping" +
-                "    FROM orderhistory" +
-                "    WHERE date BETWEEN '" + startDate + "' AND '" + endDate + "'" +
-                ")" +
-                "SELECT" +
-                "    flavor," +
-                "    topping," +
-                "    COUNT(*) AS frequency" +
-                "FROM ToppingsExploded" +
-                "WHERE topping <> ''  -- filtering out empty toppings" +
-                "GROUP BY flavor, topping" +
-                "ORDER BY frequency DESC LIMIT 2;";
-
-
-
+            String sql = "WITH ToppingsExploded AS (\r\n" + //
+                    "    -- Exploding the toppings into individual rows\r\n" + //
+                    "    SELECT date, flavor, UNNEST(STRING_TO_ARRAY(toppings, ', ')) AS topping\r\n" + //
+                    "    FROM orderhistory\r\n" + //
+                    "    WHERE date BETWEEN '\"" + startDate + "\"' AND '\" "+ endDate + "\"' \r\n" + //
+                    ")\r\n" + //
+                    "\r\n" + //
+                    "SELECT\r\n" + //
+                    "    flavor,\r\n" + //
+                    "    topping,\r\n" + //
+                    "    COUNT(*) AS frequency\r\n" + //
+                    "FROM ToppingsExploded\r\n" + //
+                    "WHERE topping <> ''  -- filtering out empty toppings\r\n" + //
+                    "GROUP BY flavor, topping\r\n" + //
+                    "ORDER BY frequency DESC;\r\n" + //
+                    "";  // Your SQL query from earlier goes here
             ResultSet resultSet = database.getData(sql);
 
-            if (resultSet == null) {
-                return new Object[][]{}; // Return empty array if there's an issue
-            }
-
-            Object[][] data = new Object[10][3]; // Adjusted size for 3 columns
+            // Convert ResultSet to Object[][]
+            // For simplicity, I'm assuming a fixed size of results.
+            // You might want to use an ArrayList if you're uncertain of the number of rows and then convert to an array.
+            Object[][] data = new Object[10][3]; // adjust the size accordingly
 
             int index = 0;
             while (resultSet.next() && index < 10) {
-                data[index][0] = resultSet.getString("flavor1") + " & " + resultSet.getString("topping1");
-                data[index][1] = resultSet.getString("flavor2") + " & " + resultSet.getString("topping2");
-                data[index][2] = resultSet.getInt("quantity"); 
+                data[index][0] = resultSet.getString("flavor");
+                data[index][1] = resultSet.getString("topping");
+                data[index][2] = resultSet.getInt("frequency");
                 index++;
             }
 
             return data;
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return new Object[][]{}; // Return empty array if there's an issue
+        return new Object[][]{}; // return empty array if there's an issue
     }
-
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
